d4c8b10e949ea7ef2845f0f4162fec28e8fdfbd6
Mohd Tajudin Saifullah Bin Abdul Wahab <m_tajudin@esyariah.gov.my>
2025-11-10 08:49:31 +0800

    Fix: Use adapter-vercel in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter()
-  }
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			runtime: 'nodejs20.x'
+		})
+	}
 };
 
 export default config;
